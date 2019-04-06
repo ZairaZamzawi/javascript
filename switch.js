a8799c60a51e27f83c438ce80cec470fdeb91ce4
Zaira Zamzawi <zairazamzawi@gmail.com>
2019-04-06 14:35:00 +0700

    Add switch.js dan cara penulisan Switch Case

---
+++ switch.js
@@ -0,0 +1,22 @@
+function testingSwitch() {
+  var nilai = 50;
+
+  // Sama seperti select case vb
+  switch (nilai) {
+    case 30:
+      console.log("Ini 30 dong nilainya");
+      break;
+    case 20:
+      console.log("Ini 20 dong nilainya");
+      break;
+    case 50:
+      console.log("Ini 50 dong nilainya");
+      break;
+    // case else di vb
+    default:
+      console.log("Ga bener nih ngasih nilainya");
+  }
+
+}
+
+testingSwitch()
