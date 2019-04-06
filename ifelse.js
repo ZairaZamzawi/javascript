95241f7c6cf5eb94e308a2031407c6c2a2cc3242
Zaira Zamzawi <zairazamzawi@gmail.com>
2019-04-06 14:18:19 +0700

    Add ifelse.js & cara penulisan IF ELSE

---
+++ ifelse.js
@@ -0,0 +1,22 @@
+function jalanSatu() {
+  var nilai = 3;
+  // penulisan if else dengan curly bracket
+  if (nilai === 3){
+      console.log("Ini Benar Angka 3");
+  }else if (nilai === 2) {
+    console.log("Kayaknya kamu ngasih nilai nya 2 deh");
+  }
+  else{
+    console.log("Bukan 3 atau 2 nih");
+  }
+
+  // penulisan if else  tanpa curly bracket
+  // if (nilai===1)console.log("OKE deh");
+  // else console.log("Boleh juga deh");
+
+  // ======================== NOTE ============================
+  // LEBIH DISARANKAN IF ELSE DENGAN CURLY bracket
+  // ==========================================================
+  }
+
+jalanSatu()
