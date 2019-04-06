71b2d4eeaca5da30d5ef41aec406bdfd74acc780
Zaira Zamzawi <zairazamzawi@gmail.com>
2019-04-06 14:07:34 +0700

    Add function.js dan cara penulisan function

---
+++ function.js
@@ -0,0 +1,19 @@
+// Tiga cara penulisan untuk function
+// cara baru
+function jalanSatu(){
+  console.log("Jalan Satu");
+}
+
+const jalanDua = function(){
+  console.log("Jalan Dua");
+}
+
+// cara lama
+const jalanTiga = ()=>{
+  console.log("Jalan Tiga");
+}
+
+// cara pemanggilan function
+jalanSatu()
+jalanDua()
+jalanTiga()
